1e781f2d614d596dd8c47534f0e3bb40f85722a5
Gino AScencio <gino.francisco@hotmail.com>
2022-07-09 23:04:39 -0500

    add: List images and use Giphy Api Key

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  private apiKey = 'Z5tvp7Y93LOpOIVifSdpOHW8jBVSvImi';
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  buscarGifs(term: string = '') {
+    term = term.trim().toLocaleLowerCase();
+    if (!this._historial.includes(term)) {
+      this._historial.unshift(term);
+      this._historial = this._historial.splice(0, 10);
+    }
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${term}&limit=10`)
+      .subscribe((resp: SearchGifsResponse) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
+  }
+}
